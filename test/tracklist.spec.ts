0ee72359f4f575c1d4826f1ba6e7f4b006be9c24
rvndev <raven-dev@outlook.com>
2023-05-25 13:16:10 +0200

    test: add tracklist/track tests

--- test/tracklist.spec.ts
+++ test/tracklist.spec.ts
@@ -1,36 +1,118 @@
 import { readFileSync } from 'fs'
-import { describe, it } from 'node:test'
 import { JSDOM } from 'jsdom'
-import { extractTrack, type Track } from '../src/tracklist'
 import assert from 'node:assert'
+import { describe, it } from 'node:test'
+import { parseTrack, parseTracklist, type Track, type Tracklist } from '../src/tracklist'
+import { omit } from './utils'
 
 describe('Track', () => {
   const html = readFileSync('./test/tracklist.html', 'utf8')
   const { document } = (new JSDOM(html)).window
 
-  it('Should extract a track from a tracklist page', () => {
+  it('should parse a fully identified track', () => {
     const track: Track = {
-      name: 'Caterina Barbieri - At Your Gamut',
-      artist: 'Caterina Barbieri',
-      publisher: 'light-years',
-      duration: 426,
+      name: 'Emotion 1',
+      artist: 'Reece Cox',
+      publisher: 'KULØR',
+      duration: 295,
       genre: 'Electronica',
-      url: 'https://www.1001tracklists.com/track/27jytsjp/caterina-barbieri-at-your-gamut/index.html'
+      url: 'https://www.1001tracklists.com/track/fpc4djf/reece-cox-emotion-1/index.html'
     }
 
-    const trackDiv = document.querySelector('[id="tlp13_content"]')
+    const selector = '[id="tlp5_content"]'
+    const trackDiv = document.querySelector(selector)
     if (trackDiv === null) {
-      throw new Error("Couldn't find the DOM element in the html file")
+      throw new Error(`No results match the ${selector} selector in the html code`)
     }
 
-    assert.deepStrictEqual(track, extractTrack(trackDiv))
+    assert.deepStrictEqual(track, parseTrack(trackDiv))
   })
 
-  it("Should throw if page doesn't contain any valid tracks", () => {
-    const nonTrackDiv = document.querySelector('[id="sBox"]')
-    if (nonTrackDiv === null) {
-      throw new Error("Couldn't find the DOM element in the html file")
+  it('should parse an unidentified track', () => {
+    const track: Track = {
+      name: 'ID',
+      artist: 'ID',
+      publisher: '',
+      duration: 0,
+      genre: '',
+      url: ''
     }
-    assert.throws(() => extractTrack(nonTrackDiv), Error)
+
+    const selector = '[id="tlp7_content"]'
+    const trackDiv = document.querySelector(selector)
+
+    if (trackDiv === null) {
+      throw new Error(`No results match the ${selector} selector in the html code`)
+    }
+
+    assert.deepStrictEqual(track, parseTrack(trackDiv))
+  })
+
+  it('should parse an unnamed track', () => {
+    const track: Track = {
+      name: 'ID',
+      artist: 'Bicep',
+      publisher: '',
+      duration: 0,
+      genre: '',
+      url: ''
+    }
+
+    const selector = '[id="tlp8_content"]'
+    const trackDiv = document.querySelector(selector)
+
+    if (trackDiv === null) {
+      throw new Error(`No results match the ${selector} selector in the html code`)
+    }
+
+    assert.deepStrictEqual(track, parseTrack(trackDiv))
+  })
+
+  it('should throw on invalid input', () => {
+    const selector = '[id="sBox"]'
+    const nonTrackDiv = document.querySelector(selector)
+
+    if (nonTrackDiv === null) {
+      throw new Error("Couldn't find find a valid track in given html")
+    }
+
+    assert.throws(() => parseTrack(nonTrackDiv))
+  })
+})
+
+describe('Tracklist', () => {
+  it('should parse a tracklist', () => {
+    const html = readFileSync('./test/tracklist.html', 'utf8')
+    const { document } = (new JSDOM(html)).window
+
+    const mockTrack: Track = {
+      name: '',
+      artist: '',
+      publisher: '',
+      duration: 0,
+      genre: '',
+      url: ''
+    }
+
+    const mockTracklist: Tracklist = {
+      name: 'Bicep - The Warehouse Project, RDM Rotterdam, Netherlands 2023-04-28',
+      artist: 'Bicep',
+      date: new Date('2023-04-28'),
+      tracks: Array(22).fill(mockTrack),
+      url: 'https://www.1001tracklists.com/tracklist/1gz59x6t/bicep-the-warehouse-project-rdm-rotterdam-netherlands-2023-04-28.html'
+    }
+
+    const parsedTracklist = parseTracklist(document)
+
+    assert.strictEqual(parsedTracklist.tracks.length, mockTracklist.tracks.length)
+
+    assert.deepStrictEqual(omit(parsedTracklist, 'tracks'), omit(mockTracklist, 'tracks'))
+  })
+
+  it('should throw on invalid input', () => {
+    const html = readFileSync('./test/captcha.html', 'utf8')
+    const { document } = (new JSDOM(html)).window
+
+    assert.throws(() => parseTracklist(document))
   })
 })
